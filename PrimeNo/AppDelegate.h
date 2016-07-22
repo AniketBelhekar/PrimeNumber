ace539ab966ac45346f826804f12b1e177f70026
Student-003 <student-003@fexli-itss-macbook-pro.local>
2016-07-22 11:27:08 +0530

    Initial Commit

---
+++ PrimeNo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PrimeNo
+//
+//  Created by Student-003 on 22/07/16.
+//  Copyright © 2016 Felix-ITs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PrimeNo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PrimeNo
+//
+//  Created by Student-003 on 22/07/16.
+//  Copyright © 2016 Felix-ITs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
